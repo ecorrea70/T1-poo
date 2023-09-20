073683b28cf0d7af706024efd45a6995d6a94eca
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-09-20 20:32:40 -0300

    Adicao dos metodos da classe ItemEstoque

--- ItemEstoque.java
+++ ItemEstoque.java
@@ -1,2 +1,36 @@
 public class ItemEstoque {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemEstoque(Produto produto, int quantidadeInicial) {
+        this.produto = produto;
+        this.quantidade = quantidadeInicial;
+    }
+
+    public boolean baixaEstoque(int baixa) {
+        if (baixa > 0 && baixa <= quantidade) {
+            quantidade -= baixa;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean reposicaoEstoque(int reposicao) {
+        if (reposicao > 0) {
+            quantidade += reposicao;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
