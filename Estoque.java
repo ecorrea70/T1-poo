3c99d76ef7c45c03988de14f51d1be8ca4fde121
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-09-20 20:32:29 -0300

    Adicao dos metodos da classe Estoque

--- Estoque.java
+++ Estoque.java
@@ -1,2 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estoque {
+    private List<ItemEstoque> itens;
+
+    public Estoque() {
+        itens = new ArrayList<>();
+    }
+
+    public int getQuantidadeDisponivel(int codigo){
+        for (ItemEstoque item : itens){
+            if (item.getProduto().getCodigo() == codigo){
+                return item.getQuantidade();
+            }
+        }
+        return 0;
+    }
+
+    public boolean baixaEstoque(int codigo, int quantidade) {
+        for (ItemEstoque item : itens) {
+            if (item.getProduto().getCodigo() == codigo) {
+                if(item.baixaEstoque(quantidade)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean reposicaoEstoque(int codigo, int quantidade) {
+        for (ItemEstoque item : itens) {
+            if (item.getProduto().getCodigo() == codigo) {
+                return item.reposicaoEstoque(quantidade);
+            }
+        }
+        return false;
+    }
+
+
+    public boolean cadastraProduto(Produto produto, int quantidade) {
+        ItemEstoque novoItem = new ItemEstoque(produto, quantidade);
+        itens.add(novoItem);
+        return true;
+    }
+
+
+    public Produto getProduto(int codigo) {
+        for (ItemEstoque item : itens) {
+            if (item.getProduto().getCodigo() == codigo) {
+                return item.getProduto();
+            }
+        }
+        return null;
+    }
+
 }
