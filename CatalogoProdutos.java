1b7f1a6531db74c03132d88e201066154d23ba2c
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-09-20 20:32:19 -0300

    Adicao dos metodos da classe CatalogoProdutos

--- CatalogoProdutos.java
+++ CatalogoProdutos.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogoProdutos {
+    private List<Produto> produtos;
+
+    public CatalogoProdutos() {
+        produtos = new ArrayList<>();
+    }
+
+    public boolean cadastraProduto(Produto produto) {
+        produtos.add(produto);
+        return true;
+    }
+
+    public Produto getProduto(int index) {
+        if (index >= 0 && index < produtos.size()) {
+            return produtos.get(index);
+        } else {
+            System.out.println("Posição inexistente na lista");
+            return null;
+        }
+    }
 }
