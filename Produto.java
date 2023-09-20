80f84620febf9a33fd99123c11e4bcc75cb2f705
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-09-20 20:32:47 -0300

    Adicao dos metodos da classe Produto

--- Produto.java
+++ Produto.java
@@ -3,6 +3,12 @@ public class Produto {
 	private String descricao;
 	private double precoUnitario;
 
+	public Produto(int codigo, String descricao, double precoUnitario) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.precoUnitario = precoUnitario;
+	}
+
 	public int getCodigo() {
 		return codigo;
 	}
